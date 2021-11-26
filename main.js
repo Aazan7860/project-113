40c095dc4583c460ce8aaf301c83bf49df6115e8
Aazan7860 <82071015+Aazan7860@users.noreply.github.com>
2021-11-26 14:46:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function draw(){
+    function take_snapshot(){
+        save(myFilter.png);
+    }
+    }
+    function preload(){
+    
+    }
+    function setup(){
+        canvas = createCanvas(300,300);
+        canvas.center();
+    }
